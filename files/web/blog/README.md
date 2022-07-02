0a6c334942e06f71cdff1c8ed88578771ed87d63
lafixier <100778884+lafixier@users.noreply.github.com>
2022-07-02 19:16:42 +0900

    add: Web/Blog 問題を追加

---
+++ files/web/blog/README.md
@@ -0,0 +1,25 @@
+# Web/Blog
+
+## 概要
+
+デザインが壊滅的な作りかけのブログアプリケーション
+
+## 問題を解くにあたって
+
+- `app/flag.txt` を直接開いてフラグを取得する必要はないです
+
+## 起動
+
+このファイル [README.md](README.md) が存在するディレクトリで以下のコマンドを実行して問題サーバを起動する (dockerが必要)
+
+```bash
+$ docker image build -t myctf-problems/web/blog:latest .
+$ docker container run -p 5000:5000 --name myctf-problems/web/blog -it myctf-problems/web/blog:latest
+```
+
+http://localhost:5000/ から問題サーバにアクセスできる
+
+## 想定解法
+
+[solveディレクトリ](solve) を参照
+
---
+++ files/web/blog/app/app.py
@@ -0,0 +1,41 @@
+import glob
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index_():
+    articles = get_articles()
+    return render_template("index.html", articles=articles)
+
+@app.route("/articles")
+def articles_():
+    path = request.args.get("title")
+    if path:
+        article = get_article(path)
+        return render_template("article.html", article=article)
+    else:
+        return redirect()
+
+def get_articles() -> list:
+    articles = []
+    for article_path in get_article_paths():
+        articles.append({"title": article_path, "path": article_path})
+    return articles
+
+def get_article_paths() -> dict:
+    article_file_paths = [path.split("/")[-1] for path in glob.glob("./articles/*")]
+    return article_file_paths
+
+def get_article(path: str) -> dict:
+    with open("./articles/"+path, encoding="utf-8") as f:
+        body = f.read()
+    return {"title": path, "body": body}
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
+
