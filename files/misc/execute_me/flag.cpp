c47db2a762d1914ae0f0b4483d9d5b88ebf74e88
lafixier <100778884+lafixier@users.noreply.github.com>
2022-07-05 16:04:09 +0900

    feat(Misc/execute_me): flagを変更

--- files/misc/execute_me/flag.cpp
+++ files/misc/execute_me/flag.cpp
@@ -5,7 +5,7 @@ std::string a();
 
 int main()
 {
-    std::string flag_1 = "Flag{chm0d_c0mmand";
+    std::string flag_1 = "Flag{run_./flag";
     std::string flag_2 = "_892743589734583829745";
     std::cout << flag_1;
     std::cout << flag_2;
